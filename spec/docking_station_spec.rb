ac6478c71f4ee35a6719be2684cbb1c5bed17860
tmgree <tmgree@outlook.com>
2017-05-11 21:00:36 +0100

    added instant variables

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,4 +11,19 @@ describe DockingStation do
     expect(bike).to be_working
   end
 
+  it { is_expected.to respond_to(:dock).with(1).argument }
+
+  it { is_expected.to respond_to(:bike) }
+
+  it 'can dock a bike' do
+    bike=Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it 'returns a docked bike' do
+    bike=Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
+
 end
