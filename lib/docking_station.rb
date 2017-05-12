a7fe82ed712907912f73769090133af9246bd215
tmgree <tmgree@outlook.com>
2017-05-12 20:59:10 +0100

    added attribute reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,14 @@ require_relative 'bike'
 
 class DockingStation
 
+  attr_reader :bike
+
   def release_bike
-    Bike.new
+    @bike
   end
 
   def dock(bike)
     @bike=bike
-  end
-
-  def bike
-    @bike
   end
 
 end
