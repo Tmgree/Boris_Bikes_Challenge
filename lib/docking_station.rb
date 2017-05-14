cbb5a56ab96199c5f4a8fdd5152de7dfa22d79b2
tmgree <tmgree@outlook.com>
2017-05-14 20:11:33 +0100

    Added preliminary capacity of 1

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,10 +5,12 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
+    fail 'No bikes available' unless @bike
     @bike
   end
 
   def dock(bike)
+    fail 'Docking Station is Full' if @bike
     @bike=bike
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,10 +6,6 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
 
-  it 'releases working bikes' do
-    bike=subject.release_bike
-    expect(bike).to be_working
-  end
 
   it { is_expected.to respond_to(:dock).with(1).argument }
 
@@ -25,5 +21,18 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.bike).to eq bike
   end
+
+  it 'raises an error when there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+  end
+
+  it 'raises an error when a second bike it docked' do
+    bike1=Bike.new
+    bike2=Bike.new
+    subject.dock(bike1)
+    expect { subject.dock(bike2) }.to raise_error 'Docking Station is Full'
+  end
+
+
 
 end
