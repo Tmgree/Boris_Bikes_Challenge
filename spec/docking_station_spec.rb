d9aeb00875442086ad6c8cd955a8f137b3353514
tmgree <tmgree@outlook.com>
2017-05-15 20:06:18 +0100

    added broken functionality to bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -40,5 +40,12 @@ describe DockingStation do
     50.times { station.dock(Bike.new) }
     expect{ station.dock(Bike.new) }.to raise_error 'Docking Station is Full'
   end
-  
+
+  it 'must not release a broken bike' do
+    bike=Bike.new
+    bike.report_broken
+    subject.dock(bike)
+    expect(subject.release_bike).to raise_error 'Bike is Broken'
+  end
+
 end
