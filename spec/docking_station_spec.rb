4f5baaddc95110aa7be78e04e50b4d3e9b3c2b68
tmgree <tmgree@outlook.com>
2017-05-18 20:34:53 +0100

    organised tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,83 +2,141 @@ require 'docking_station'
 
 describe DockingStation do
 
-  it 'responds to release_bike' do
-    expect(subject).to respond_to :release_bike
+
+  describe '#release_bike' do
+
+    it 'responds to release_bike' do
+      expect(subject).to respond_to :release_bike
+    end
+
+    it 'raises an error when there are no bikes available' do
+        expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+
+    it 'must not release a broken bike' do
+      bike=Bike.new
+      bike.report_broken
+      subject.dock(bike)
+      expect{subject.release_bike}.to raise_error 'No bikes available'
+    end
+
   end
 
+  describe '#dock' do
 
-  it { is_expected.to respond_to(:dock).with(1).argument }
+    it { is_expected.to respond_to(:dock).with(1).argument }
 
-  it { is_expected.to respond_to(:bikes) }
+    it 'can dock a bike' do
+      bike=Bike.new
+      expect(subject.dock(bike)).to eq [bike]
+    end
 
-  it 'can dock a bike' do
-    bike=Bike.new
-    expect(subject.dock(bike)).to eq [bike]
+    it 'allows the user to set their own capacity' do
+      station = DockingStation.new(50)
+      50.times { station.dock(Bike.new) }
+      expect{ station.dock(Bike.new) }.to raise_error 'Docking Station is Full'
+    end
+
   end
 
-  it 'returns a docked bike' do
-    bike=Bike.new
-    subject.dock(bike)
-    expect(subject.bikes[0]).to eq bike
+  describe '#full?' do
+
+    it 'should identify when the dock is full' do
+      station=DockingStation.new(1)
+      bike=Bike.new
+      station.dock(bike)
+      expect(station.full?).to eq(true)
+    end
+
   end
 
+  describe '#empty?' do
 
-  it 'raises an error when there are no bikes available' do
-      expect { subject.release_bike }.to raise_error 'No bikes available'
+    it 'should identify when the dock is empty' do
+      station=DockingStation.new
+      expect(station.empty?).to eq(true)
+    end
+
   end
 
-  it 'raises an error when carrying capacity is reached' do
-    DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
-    expect { subject.dock(Bike.new) }.to raise_error 'Docking Station is Full'
+  describe '#dock_size' do
+
+    it 'Must display the number of bikes stored in a docking station' do
+      station = DockingStation.new(28)
+      25.times { station.dock(Bike.new) }
+      expect( station.dock_size ).to eq(25)
+    end
+
   end
 
-  it 'has a default capacity' do
-    expect( subject.capacity ).to eq DockingStation::DEFAULT_CAPACITY
+  describe 'no_working_bikes?' do
+
+    it 'must identify that there are no working bikes docked' do
+      bike=Bike.new
+      bike.report_broken
+      subject.dock(bike)
+      expect(subject.no_working_bikes?).to eq(true)
+    end
+
   end
 
-  it 'allows the user to set their own capacity' do
-    station = DockingStation.new(50)
-    50.times { station.dock(Bike.new) }
-    expect{ station.dock(Bike.new) }.to raise_error 'Docking Station is Full'
+  describe '#add_to_counter' do
+
+    it 'must add 1 to working bikes if a working bike is docked' do
+      bike=Bike.new
+      expect(subject.add_to_counter(bike)).to eq(1)
+    end
+
+    it 'must add 1 to broken bikes if a broken bike is docked' do
+      bike=Bike.new
+      bike.report_broken
+      expect(subject.add_to_counter(bike)).to eq(1)
+    end
+
   end
 
-  it 'Must display the number of bikes stored in a docking station' do
-    station = DockingStation.new(28)
-    25.times { station.dock(Bike.new) }
-    expect( station.dock_size ).to eq(25)
+  describe '#bikes' do
+
+    it { is_expected.to respond_to(:bikes) }
+
+    it 'returns a docked bike' do
+      bike=Bike.new
+      subject.dock(bike)
+      expect(subject.bikes[0]).to eq bike
+    end
+
   end
 
-  it 'must count number of working bikes stored in docking station' do
-    18.times { subject.dock(Bike.new) }
-    expect(subject.working_bikes).to eq(18)
+  describe '#capacity' do
+
+    it { is_expected.to respond_to(:capacity) }
+
+    it 'has a default capacity' do
+      expect( subject.capacity ).to eq DockingStation::DEFAULT_CAPACITY
+    end
+
   end
 
-  it 'must count number of broken bikes stored in docking station' do
-    18.times { subject.dock(Bike.new) }
-    expect(subject.broken_bikes).to eq(0)
+  describe '#working_bikes' do
+
+    it { is_expected.to respond_to(:working_bikes) }
+
+    it 'must count number of working bikes stored in docking station' do
+      18.times { subject.dock(Bike.new) }
+      expect(subject.working_bikes).to eq(18)
+    end
+
   end
 
-  it 'must not release a broken bike' do
-    bike=Bike.new
-    bike.report_broken
-    subject.dock(bike)
-    expect(subject.release_bike).to raise_error 'Bike is Broken'
-  end
+  describe '#broken_bikes' do
 
-  it 'must identify a working bike' do
-    15.times { subject.dock(Bike.new) }
-    bike=Bike.new
-    bike.report_broken
-    subject.dock(bike)
-    3.times { subject.dock(Bike.new) }
-    expect(subject.identify_working_bike).to eq(15)
-  end
+    it { is_expected.to respond_to(:broken_bikes) }
 
-  it 'must identify that there are no working bikes docked' do
-    bike=Bike.new
-    bike.report_broken
-    subject.dock(bike)
-    expect(subject.no_working_bikes?).to eq(true)
+    it 'must count number of broken bikes stored in docking station' do
+      18.times { subject.dock(Bike.new) }
+      expect(subject.broken_bikes).to eq(0)
+    end
+
   end
 
 end
