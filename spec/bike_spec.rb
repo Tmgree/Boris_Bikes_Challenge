1178daefc0fdc69717d7884bf4c2f403676e4549
tmgree <tmgree@outlook.com>
2017-05-20 20:06:53 +0100

    added doubles to tests

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,11 +2,34 @@ require 'bike'
 
 describe Bike do
 
-  it { is_expected.to respond_to :working?}
+  describe '#broken'do
 
-  it 'can be reported broken' do
-    subject.report_broken
-    expect(subject).to be_broken
+    it'should initialize as false' do
+      expect(subject.broken).to eq(false)
+    end
+
+  end
+
+  describe '#report_broken' do
+
+    it 'can be reported broken' do
+      subject.report_broken
+      expect(subject).to be_broken
+    end
+
+  end
+
+  describe '#broken?' do
+
+    it 'should return false if bike is working' do
+      expect(subject.broken?).to eq(false)
+    end
+
+    it 'should return true if bike is broken' do
+      subject.report_broken
+      expect(subject.broken?).to eq(true)
+    end
+
   end
 
 end
